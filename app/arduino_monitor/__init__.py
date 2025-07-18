3f1afc88f5375b74deba9ec18dc6956af11811be
valcalepi <32712566+valcalepi@users.noreply.github.com>
2025-07-18 11:39:49 -0400

    Delete app directory

--- app/arduino_monitor/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Arduino Monitor package for Deep Blue Pool Chemistry.
-
-This package provides functionality for monitoring Arduino sensors.
-"""
-
-# Import will be handled by the parent module
